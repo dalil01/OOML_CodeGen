3bb7ae1a200ca154cf63984dcd891653bb5173b1
Dalil CHABLIS <dalil.chablis@gmail.com>
2023-06-13 17:24:54 +0200

    Started generator

---
+++ src/main/java/com/ooml_codegen/generator/all/JavaGenerator.java
@@ -0,0 +1,12 @@
+package com.ooml_codegen.generator.all;
+
+import com.ooml_codegen.generator.Generator;
+
+public class JavaGenerator extends Generator {
+
+	@Override
+	public void generate() {
+
+	}
+
+}
