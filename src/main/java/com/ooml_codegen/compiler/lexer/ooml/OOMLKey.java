87c4a1e654f60e497de303835967308abd2ec68a
Celivalg <alex.coutansais@gmail.com>
2023-06-14 12:06:13 +0200

    Used a enum for symbols... don't know if this helped readability or crippled it...

--- src/main/java/com/ooml_codegen/compiler/lexer/ooml/OOMLKey.java
+++ src/main/java/com/ooml_codegen/compiler/lexer/ooml/OOMLKey.java
@@ -2,30 +2,53 @@ package com.ooml_codegen.compiler.lexer.ooml;
 
 public enum OOMLKey {
 
-    NEWLINE("\n\r\u2028\u2029\u0085"),
-    SPACE(" \t"),
-    SINGLE_QUOTE("'"),
-    DOUBLE_QUOTE("\""),
-    BACK_QUOTE("`"),
-    QUOTE(SINGLE_QUOTE.value + DOUBLE_QUOTE.value + BACK_QUOTE.value),
-    EQUAL("="),
-    ACCESS_MODIFIER("+-#"),
-    INHERITANCE("->"),
-    COMMA(","),
-    COLON(":"),
-    IMPORT("@"),
+    NEWLINE(OOMLSymbols.NEWLINE.stringValue() +
+            OOMLSymbols.NEXT_LINE.stringValue() +
+            OOMLSymbols.RETURN_CARRIAGE.stringValue() +
+            OOMLSymbols.LINE_SEPARATOR.stringValue() +
+            OOMLSymbols.PARAGRAPH_SEPARATOR.stringValue()),
+    SPACE(OOMLSymbols.SPACE.stringValue() +
+            OOMLSymbols.TABULATION.stringValue()),
+    QUOTE(OOMLSymbols.SINGLE_QUOTE.stringValue() +
+            OOMLSymbols.DOUBLE_QUOTE.stringValue() +
+            OOMLSymbols.BACK_QUOTE.stringValue()),
+    ACCESS_MODIFIER(OOMLSymbols.PLUS.stringValue() +
+            OOMLSymbols.MINUS.stringValue() +
+            OOMLSymbols.HASH.stringValue()),
+
+    PARENTHESIS(OOMLSymbols.OPENING_PARENTHESIS.stringValue() +
+            OOMLSymbols.CLOSING_PARENTHESIS.stringValue()),
+    CURLY_BRACKET(OOMLSymbols.OPENING_CURLY_BRACKET.stringValue() +
+            OOMLSymbols.CLOSING_CURLY_BRACKET.stringValue()),
+    BRACKET(OOMLSymbols.OPENING_BRACKET.stringValue() +
+            OOMLSymbols.CLOSING_BRACKET.stringValue()),
 
     PACKAGE_S("package"),
 
     PAD(SPACE.value + NEWLINE.value),
 
-    WORD_END(PAD.value + COLON.value +
-            QUOTE.value + EQUAL.value +
-            IMPORT.value + ACCESS_MODIFIER.value + COMMA.value + "/{()}[]"),
+    WORD_END(PAD.value +
+            OOMLSymbols.COLON.stringValue() +
+            QUOTE.value +
+            OOMLSymbols.EQUAL.stringValue() +
+            OOMLSymbols.IMPORT.stringValue() +
+            ACCESS_MODIFIER.value +
+            OOMLSymbols.COMMA.stringValue() +
+            OOMLSymbols.SLASH.stringValue() +
+            CURLY_BRACKET.value +
+            PARENTHESIS.value +
+            BRACKET.value),
 
-    FILE_END(PAD.value + COLON.value +
-            QUOTE.value + EQUAL.value +
-            IMPORT.value + ACCESS_MODIFIER.value + COMMA.value + "{()}[]"),
+    FILE_END(PAD.value +
+            OOMLSymbols.COLON.stringValue() +
+            QUOTE.value +
+            OOMLSymbols.EQUAL.stringValue() +
+            OOMLSymbols.IMPORT.stringValue() +
+            ACCESS_MODIFIER.value +
+            OOMLSymbols.COMMA.stringValue() +
+            CURLY_BRACKET.value +
+            PARENTHESIS.value +
+            BRACKET.value),
     ;
 
     private final String value;
@@ -38,8 +61,5 @@ public enum OOMLKey {
         return this.value;
     }
 
-    public char getCharValue() {
-        return this.value.charAt(0);
-    }
 
 }
