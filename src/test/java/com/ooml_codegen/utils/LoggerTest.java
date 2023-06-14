55f504f9579a8164ed9b5b3ea455b664d07afeeb
Celivalg <alex.coutansais@gmail.com>
2023-06-14 13:52:46 +0200

    Fixed System.out not being reset properly at the end of test (doesn't impact much, as afaik the testing framework fixes it anyway? but allows printing to out in tests)

--- src/test/java/com/ooml_codegen/utils/LoggerTest.java
+++ src/test/java/com/ooml_codegen/utils/LoggerTest.java
@@ -2,6 +2,8 @@ package com.ooml_codegen.utils;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import org.junit.jupiter.api.Assertions;
@@ -16,71 +18,84 @@ public class LoggerTest {
     @Test
     public void traceTest() {
         ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        // System.out needs to be stored before changing the System stream
+        // as System.out refers to the current output stream and not the default one.
+        PrintStream out = System.out;
         System.setOut(new PrintStream(outputStream));
 
         Logger.trace(error);
 
         String output = outputStream.toString().trim();
+        System.setOut(out);
+
         String expectedOutput = new Date() + " TRACE " + error;
 
         Assertions.assertEquals(expectedOutput, output);
-        System.setOut(System.out);
     }
 
     @Test
     public void debugTest() {
         ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        PrintStream out = System.out;
         System.setOut(new PrintStream(outputStream));
 
         Logger.debug(error);
 
         String output = outputStream.toString().trim();
+        System.setOut(out);
+
         String expectedOutput = new Date() + " DEBUG " + error;
 
         Assertions.assertEquals(expectedOutput, output);
-        System.setOut(System.out);
+
     }
 
     @Test
     public void infoTest() {
         ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        PrintStream out = System.out;
         System.setOut(new PrintStream(outputStream));
 
         Logger.info(error);
 
         String output = "\u001B" + outputStream.toString().trim();
+        System.setOut(out);
+
         String expectedOutput = ColorCode.GREEN + new Date().toString() + " INFO " + error + ColorCode.RESET;
 
         Assertions.assertEquals(expectedOutput, output);
-        System.setOut(System.out);
     }
 
     @Test
     public void warnTest() {
         ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        PrintStream out = System.out;
         System.setOut(new PrintStream(outputStream));
 
         Logger.warn(error);
 
         String output = "\u001B" + outputStream.toString().trim();
+        System.setOut(out);
+
         String expectedOutput = ColorCode.YELLOW + new Date().toString() + " WARN " + error + ColorCode.RESET;
 
         Assertions.assertEquals(expectedOutput, output);
-        System.setOut(System.out);
     }
 
     @Test
     public void errorTest() {
         ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        PrintStream out = System.out;
         System.setOut(new PrintStream(outputStream));
 
         Logger.error(error);
 
         String output = "\u001B" + outputStream.toString().trim();
+        System.setOut(out);
+
         String expectedOutput = ColorCode.RED + new Date().toString() + " ERROR " + error + ColorCode.RESET;
 
         Assertions.assertEquals(expectedOutput, output);
-        System.setOut(System.out);
     }
 
 }
