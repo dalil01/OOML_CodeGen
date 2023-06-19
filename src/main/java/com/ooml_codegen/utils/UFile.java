8f01cba7e5fdd81a04145b8a93d1687e181cd213
Dalil CHABLIS <dalil.chablis@gmail.com>
2023-06-19 20:04:42 +0200

    Add findOOMLFilesPath utility method

---
+++ src/main/java/com/ooml_codegen/utils/UFile.java
@@ -0,0 +1,50 @@
+package com.ooml_codegen.utils;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+public class UFile {
+
+	public static List<String> findOOMLFilesPath(String mainPath) {
+		List<String> list = new ArrayList<>();
+
+		File file = new File(mainPath);
+
+		if (!file.exists()) {
+			// TODO: throw error ?
+			return list;
+		}
+
+		if (file.isDirectory()) {
+			Stack<File> stack = new Stack<>();
+			stack.push(file);
+
+			while (!stack.empty()) {
+				File currentFile = stack.pop();
+
+				if (currentFile.isDirectory()) {
+					File[] files = currentFile.listFiles();
+					if (files != null) {
+						for (File f : files) {
+							stack.push(f);
+						}
+					}
+				} else if (isOOMLFile(currentFile.getAbsolutePath())) {
+					list.add(currentFile.getAbsolutePath());
+				}
+			}
+		}
+		else if (isOOMLFile(mainPath)) {
+			list.add(file.getAbsolutePath());
+		}
+
+		return list;
+	}
+
+	public static boolean isOOMLFile(String filePath) {
+		return filePath.endsWith(".ooml");
+	}
+
+}
---
+++ src/test/java/com/ooml_codegen/utils/UFileTest.java
@@ -0,0 +1,41 @@
+package com.ooml_codegen.utils;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class UFileTest {
+
+	private final static String pathPrefix = normalizedPath(System.getProperty("user.dir") + "/src/test/java/com/ooml_codegen/utils/ooml/");
+
+	@Test
+	public void findOOMLFilesPathTest() {
+		List<String> emptyDir = UFile.findOOMLFilesPath(pathPrefix + "empty");
+		Assertions.assertTrue(emptyDir.isEmpty());
+
+		List<String> invalidOOMLFile = UFile.findOOMLFilesPath(pathPrefix + "empty/empty");
+		Assertions.assertTrue(invalidOOMLFile.isEmpty());
+
+		String mainOOMLPath = pathPrefix + "main.ooml";
+		List<String> foundMainOOML = UFile.findOOMLFilesPath(mainOOMLPath);
+		Assertions.assertEquals(1, foundMainOOML.size());
+		Assertions.assertEquals(mainOOMLPath, normalizedPath(foundMainOOML.get(0)));
+
+		List<String> OOMLFilesInDir = UFile.findOOMLFilesPath(pathPrefix + "files");
+		Assertions.assertEquals(4, OOMLFilesInDir.size());
+		OOMLFilesInDir = OOMLFilesInDir.stream().map(UFileTest::normalizedPath).collect(Collectors.toList());
+		Assertions.assertTrue(OOMLFilesInDir.contains(pathPrefix + "files/main.ooml"));
+		String srcPath = pathPrefix + "files/src/";
+		Assertions.assertTrue(OOMLFilesInDir.contains(srcPath + "controllers.ooml"));
+		Assertions.assertTrue(OOMLFilesInDir.contains(srcPath + "models.ooml"));
+		Assertions.assertTrue(OOMLFilesInDir.contains(srcPath + "services.ooml"));
+	}
+
+	public static String normalizedPath(String s) {
+		return s.replaceAll("\\\\", "/");
+	}
+
+
+}
