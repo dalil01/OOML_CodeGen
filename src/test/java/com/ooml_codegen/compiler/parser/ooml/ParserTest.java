d74459d7d2d46ca714676abc4a598ebf1a8c6cc5
Dalil CHABLIS <dalil.chablis@gmail.com>
2023-06-20 17:17:57 +0200

    start parser test

---
+++ src/test/java/com/ooml_codegen/compiler/parser/ooml/ParserTest.java
@@ -0,0 +1,21 @@
+package com.ooml_codegen.compiler.parser.ooml;
+
+import com.ooml_codegen.compiler.lexer.ooml.OOMLLexerManager;
+import com.ooml_codegen.compiler.parser.Parser;
+import org.junit.jupiter.api.Test;
+
+import java.io.FileNotFoundException;
+
+public class ParserTest {
+
+	private final static String pathPrefix = System.getProperty("user.dir") + "/src/test/java/com/ooml_codegen/compiler/parser/ooml/files/";
+
+	@Test
+	public void parseTest() throws FileNotFoundException {
+		OOMLLexerManager lexer = new OOMLLexerManager(pathPrefix + "main.ooml");
+		Parser parser = new OOMLParser(lexer);
+
+		parser.parse();
+	}
+
+}
