336247ccaf946e7c6d9e1419696c1d7e1125758c
AbrahamDiaw <diawabrahampro@gmail.com>
2023-06-20 17:14:41 +0200

    wip enum generator

--- src/test/java/com/ooml_codegen/models/EnumTest.java
+++ src/test/java/com/ooml_codegen/models/EnumTest.java
@@ -1,4 +1,25 @@
 package com.ooml_codegen.models;
 
+import com.ooml_codegen.models.enums.modifiers.access.EnumAccessModifier;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 public class EnumTest {
+    private final String expectedName = "Color";
+    private final Package expectedPackage = new Package("com.ooml_codegen.models");
+    private final EnumAccessModifier accessModifier = EnumAccessModifier.PUBLIC;
+    private Enum anenum;
+
+    @BeforeEach
+    public void setup() {
+        this.anenum = new Enum(this.expectedName, this.expectedPackage, this.accessModifier);
+    }
+
+    @Test
+    public void constructorTest() {
+        Assertions.assertEquals(this.expectedName, this.anenum.getName());
+        Assertions.assertEquals(this.expectedPackage, this.anenum.getPackage());
+        Assertions.assertEquals(this.accessModifier, this.anenum.getAccessModifier());
+    }
 }
