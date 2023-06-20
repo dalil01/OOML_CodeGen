574ab259de9832d3096128f00f41e45e5f5730ea
Celivalg <alex.coutansais@gmail.com>
2023-06-20 11:41:37 +0200

    More cleanup

--- src/main/java/com/ooml_codegen/compiler/lexer/Lexer.java
+++ src/main/java/com/ooml_codegen/compiler/lexer/Lexer.java
@@ -1,6 +1,5 @@
 package com.ooml_codegen.compiler.lexer;
 
-import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayDeque;
 import java.util.Deque;
@@ -9,11 +8,35 @@ import java.util.Deque;
 public abstract class Lexer {
 
 	protected Deque<SingleLexer> stack;
-	protected Lexer(SingleLexer lexer) throws FileNotFoundException {
+	protected Lexer(SingleLexer lexer) {
 		this.stack = new ArrayDeque<>();
 		stack.push(lexer);
 	}
 
-	public abstract Token nextToken() throws FileNotFoundException;
+	public Token nextToken() throws FileNotFoundException{
+		if (!stack.isEmpty()){
+			Token tok = this.stack.peek().nextToken();
+
+			while (tok.getType() == TokenType.EOF){
+				this.stack.pop();
+
+				if (this.stack.isEmpty()) {
+					break;
+				} else {
+					tok = this.stack.peek().nextToken();
+				}
+			}
+
+			if (tok.getType() == TokenType.IMPORT){
+				manageImport(tok);
+				return nextToken();
+			}
+
+			return tok;
+		}
+		return new Token(TokenType.EOF);
+	}
+
+	protected abstract void manageImport(Token token) throws FileNotFoundException;
 
 }
