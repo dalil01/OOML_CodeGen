be3e67089d0ddfc43cae21f5c80a87f475e23d3a
Celivalg <alex.coutansais@gmail.com>
2023-06-20 12:41:42 +0200

    Made SingleLexer package-private

--- src/main/java/com/ooml_codegen/compiler/lexer/ooml/OOMLLexer.java
+++ src/main/java/com/ooml_codegen/compiler/lexer/ooml/OOMLLexer.java
@@ -1,18 +1,15 @@
 package com.ooml_codegen.compiler.lexer.ooml;
 
 import com.ooml_codegen.compiler.lexer.Lexer;
-import com.ooml_codegen.compiler.lexer.SingleLexer;
 import com.ooml_codegen.compiler.lexer.Token;
 import com.ooml_codegen.utils.Logger;
 import com.ooml_codegen.utils.UFile;
 
 import java.util.List;
 import java.io.FileNotFoundException;
-import java.util.Deque;
 
 public class OOMLLexer extends Lexer {
 
-    Deque<SingleLexer> stack;
     public OOMLLexer(String file)  throws FileNotFoundException {
         super(new OOMLSingleLexer(file));
     }
