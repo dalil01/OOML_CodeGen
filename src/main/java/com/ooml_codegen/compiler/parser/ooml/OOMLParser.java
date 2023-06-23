0706b5943a6f3111e65c619ac677fcbf0b52279d
Dalil CHABLIS <dalil.chablis@gmail.com>
2023-06-23 23:42:57 +0200

    add added an error message

--- src/main/java/com/ooml_codegen/compiler/parser/ooml/OOMLParser.java
+++ src/main/java/com/ooml_codegen/compiler/parser/ooml/OOMLParser.java
@@ -4,6 +4,7 @@ import com.ooml_codegen.compiler.lexer.Token;
 import com.ooml_codegen.compiler.lexer.TokenType;
 import com.ooml_codegen.compiler.lexer.ooml.OOMLLexerManager;
 import com.ooml_codegen.compiler.parser.Parser;
+import com.ooml_codegen.utils.ULogger;
 
 import java.io.FileNotFoundException;
 
@@ -35,8 +36,9 @@ public class OOMLParser extends Parser {
 	private void parsePackage() throws FileNotFoundException {
 		Token nextToken = this.lexerManager.nextToken();
 
-		if (nextToken.getType() != TokenType.WORD) {
-			// TODO mange error
+		if (nextToken.getType() != TokenType.WORD && nextToken.getType() != TokenType.QUOTED_WORD) {
+			ULogger.error("Invalid package statement at " + nextToken.getLocation());
+			// TODO: need to throw the error, but need a proper exception class for that
 			return;
 		}
 	}
